cbb4084be457b1c8062a5b6c045dd7517e4db861
Daniel Arizu Pascual <67865575+dapser75@users.noreply.github.com>
2020-09-21 09:03:38 +0200

    Add files via upload

--- Fase_1.java
+++ Fase_1.java
@@ -7,23 +7,46 @@ package modulo_2;
 import javax.swing.JOptionPane;
 
 class Fase_1 {
+	//Declaración de variables
+	private char nombre[]= {'D','A','N','I','E','L',' ','A','R','I','Z','U'};
+	private String name="";
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char[] getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(char[] nombre) {
+		this.nombre = nombre;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			//Declaración de variables
-			char nombre[]= {'D','A','N','I','E','L',' ','A','R','I','Z','U'};
+		//Declaración de variables
+		Fase_1 d=new Fase_1();
+		
 			//Imprimir caracter a caracter, recorriendo bucle FOR
-			for (int i=0;i<nombre.length;i++) {
-				System.out.println(nombre[i]);
+			for (int i=0;i<d.getNombre().length;i++) {
+				System.out.println(d.getNombre()[i]);
 				
 			}
+		
+			System.out.println("\n" + "Prueba entrada nombre por teclado");
 			//Prueba personal para hacer la entrada por comandos
-			String name=JOptionPane.showInputDialog("Introduce tu nombre: ");
-			System.out.print("\n");
-			char [] nom=name.toCharArray(); //Conversión de string a Array
-			for (int i=0;i<nom.length;i++) {
-				System.out.print(nom[i]+ " ");
+			d.setName(JOptionPane.showInputDialog("Introduce tu nombre: "));
+			d.setNombre(d.name.toCharArray());//Conversión de string a Array
+			//Imprimir caracter a caracter, recorriendo bucle FOR
+			for (int i=0; i< d.getNombre().length;i++) {
+				System.out.print(d.getNombre()[i]+ " ");
 								
 			}
-		}
+	}
 
 }
